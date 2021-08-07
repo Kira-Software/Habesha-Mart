f05ee5b22adf5de8a389aac211588b86bcf5780b
enenahom <enenahom@gmail.com>
2021-08-07 15:18:49 +0300

    serch sort filter add completed

--- backendHabesha/controllers/item-controller.js
+++ backendHabesha/controllers/item-controller.js
@@ -43,26 +43,41 @@ exports.findItem=catchAsync(async(req,res,next)=>{
 
         //ADVANCED FILTERING
         let queryStr=JSON.stringify(queryObj);
-        queryStr=queryStr.replace(/\b(gte|gt|lte|lt) \b/g, match => `$${match}`); //this is for in ur query u put like duration[gte]=5 new emtelut keza
-                                                                                       // queryObj {duration :{gte:'5'}} hone but '$gte' neber mehon yalbet so ene gte eyflege $ mechemer
-        let query=Item.find(JSON.parse(queryStr));
-
+        query=queryStr.replace(/\b(gte|gt|lte|lt) \b/g, match => `$${match}`); //this is for in ur query u put like duration[gte]=5 new emtelut keza
+                                                                                          // queryObj {duration :{gte:'5'}} hone but '$gte' neber mehon yalbet so ene gte eyflege $ mechemer
         //SORTING
         if(req.query.sort){
-           const sortBy=req.query.sort.split(',').join(' ');   //?sort('price','rating') belehe yelakewn price rating belo leyebehca simultinius execute yagal b sort
-           query=query.sort(sortBy)  
-        }else{
-           query=query.sort('-createdAt'); //yehe demo menem aynet sort kaltera b default adisochun kela jemero yedredral
+         const sortBy=req.query.sort.split(',').join(' ');   //?sort('price','rating') belehe yelakewn price rating belo leyebehca simultinius execute yagal b sort
+         query=query.sort(sortBy)  
+      }else{
+         query=query.sort('-createdAt'); //yehe demo menem aynet sort kaltera b default adisochun kela jemero yedredral
+      }
+        //limiting fields
+        if(req.query.fields){
+           const fields=req.query.fields.split(',').join(' ');
+           query=query.select(fields);
+        }else
+        {
+           query=query.select('-__v');
         }
+        //pagination
+        const page=req.query.page * 1 || 1;
+        const limit =req.query.limit * 1 || 25;  //25 items displayed initialy
+        const skip=(page-1) * limit;
+        query=query.skip(skip).limit(limit);
         
-
+        if(req.query.page){
+            const numOfItems=await Item.countDocuments();
+            if(skip >= numOfItems){
+               return new AppError("No More items are available !")
+            }
+        }
         
         
        //EXECUTE A QUERY
-        const item=await query; // melseh wede object keyerena felega ketel
+        const item=await Item.find(JSON.parse(queryStr)); // melseh wede object keyerena felega ketel
 
         //SEND RESPONSE
-
         res.status(200).json({status:'success',
                               results:item.length(),
                               data:item});
